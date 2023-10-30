7f0697c88c3f36c7d47a5f1e045a2c8f5913b711
SavanDhola1810 <123728753+SavanDhola1810@users.noreply.github.com>
2023-10-30 21:20:11 +0530

    second commit

--- components/JobList.jsx
+++ components/JobList.jsx
@@ -31,7 +31,7 @@ const JobList = () => {
                             <div className='mt-2 text-gray-700 text-sm sm:text-base'>
                                 Already have both html and css code, in need of a skiller person to re-write fresh and new css code that is easy to read but responsive and eye catching. The css code must not be too advanced and must be clean. More details once hired. Please do not respond if you dont have the required skills as this is timely project that needs to be completed as soon as possible.
 
-                                moreabout "Develop a relatively basic but eye-catching css code for a web-page"
+                                moreabout Develop a relatively basic but eye-catching css code for a web-page
                             </div>
                             <button className=' mt-6  rounded-md  text-white  bg-gradient-to-r from-cyan-400 to-blue-400 buttonHover tracking-wider w-[120px] sm:w-[130px] md:w-[140px] lg:w-[150px]  h-10 ' >Apply</button>
                         </div>
